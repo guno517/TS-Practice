9cddd1ba0311963cb2c877e6a6f3192f217c2304
guno517 <60571418+guno517@users.noreply.github.com>
2021-09-07 14:55:59 +0900

    interface 사용과 readonly 사용

--- app.ts
+++ app.ts
@@ -1,30 +1,30 @@
-type Store = { // 타입 알리아스 type alias
+interface Store { // 타입 알리아스 type alias
     currentPage: number; // 세미콜론
     feeds: NewsFeed[];
 }
 
-type News = {
-    id: number;
-    time_ago: string;
-    title: string;
-    url: string;
-    user: string;
-    content: string;
+interface News {
+    readonly id: number; // readonly: 변경 불가 기능
+    readonly time_ago: string;
+    readonly title: string;
+    readonly url: string;
+    readonly user: string;
+    readonly content: string;
 }
 
-type NewsFeed = News & { // intersection 기능 (중복 생략 가능)
-    comments_count: number;
-    points: number;
+interface NewsFeed extends News { // intersection 기능 (중복 생략 가능)
+    readonly comments_count: number;
+    readonly points: number;
     read?: boolean; // ?는 선택 속성을 의미
 }
 
-type NewsDetail = News & {
-    comments: NewsComment[];
+interface NewsDetail extends News {
+    readonly comments: NewsComment[];
 }
 
-type NewsComment = News & {
-    comments: NewsComment[];
-    level: number;
+interface NewsComment extends News {
+    readonly comments: NewsComment[];
+    readonly level: number;
 }
 
 const container: HTMLElement | null = document.getElementById("root");
