db798e499a40dd9a9d8ba5814ee59c90caa96d26
guno517 <60571418+guno517@users.noreply.github.com>
2021-09-13 14:11:53 +0900

    URL 분리

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"; // 해커 뉴스 news 1페이지
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json"; // @id를 통해 뉴스 기사 고유의 id를 파악해 해당 뉴스 기사의 json을 가져온다
